5c234dbf73285bc5852727b597fcd5a168ce3c90
Evgeniy Shpilco <callelia@yandex.ru>
2024-09-21 00:43:00 +0600

    creating a shared list of movies

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,7 +1,30 @@
-function Main() {
-    return <main className="container content">
-        Hello From Movies App
-    </main>
+import React from 'react'
+import {Movies} from '../components/Movies'
+
+class Main extends React.Component {
+    state = {
+        movies: [],
+    }
+
+    componentDidMount() {
+        fetch('http://www.omdbapi.com/?apikey=78584b3c&s=matrix')
+            .then(response => response.json())
+            .then(data => this.setState({ movies: data.Search || [] }))
+            
+    }    
+
+    render() {
+        const {movies} = this.state;
+
+        return <main className="container content">
+            {
+                movies.length ? (
+                    <Movies movies={this.state.movies}/>
+                ) : <h3>Loading...</h3>
+            }
+            <Movies movies={this.state.movies}/>
+        </main>
+    }
 }
 
 export {Main}
